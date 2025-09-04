093c8e93573176ac0309ba5f2b3d032cbaef7061
Lazy-CSharp-Coder <haraldn.kodeloftet@gmail.com>
2025-09-04 10:51:51 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Start-Javascript-Template
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/Lazy-CSharp-Coder/Start-Javascript-Template)
---
+++ script.js
@@ -0,0 +1,25 @@
+console.log('Hello World');
+
+// Variabler / Variables
+
+/* 
+const - nøkkelord for å erklære en variabel
+userName - funnet på variabel navn
+= setter verdien på variabel
+"Julie" - string verdi til variabel
+
+*/
+
+const userName = "'Harald"; // string variabel
+console.log(userName); // skjekk variabel
+
+let userAge = 28; // number
+let isPresent = true; // boolean
+
+// var isPresent = true;  // er gått ut på dato - støttes fremdeles
+
+// Regular Function
+
+function character() {
+  console.log("Er inne i funskjon character()");
+}
